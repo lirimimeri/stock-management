570065b0d5ce40b695b4193929da12247d35c1f6
Lirim <lirimimeri17@gmail.com>
2023-12-03 21:46:54 +0100

    server: Created products endpoints, added cors.

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173', // replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(4000);
-  console.log('app started');
 }
 bootstrap();
